024c1e34ba1e66431f66acd8dea8cb7f0220e978
Mohamed Asir <asir.abm@gmail.com>
2023-03-29 13:15:59 +0530

    connect to client

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const express=require('express')
 const app=express()
+const bodyparser=require('body-parser')
+const cors=require('cors')
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.use(cors())
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.json({
         name:'asir',
         age:31
     })
 })
+app.post('/reg',(req,res)=>{
+    console.log('heloooo')
+    const user=req.body.user
+    console.log(user)
+})
+
+
+
+
 
 
 app.listen(4000,()=>console.log('connection established sucessfully'))
