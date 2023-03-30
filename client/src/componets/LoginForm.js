d35b22b089fc91278a638053d35a253b272d8030
Mohamed Asir <asir.abm@gmail.com>
2023-03-30 12:58:57 +0530

    almost finised

--- client/src/componets/LoginForm.js
+++ client/src/componets/LoginForm.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import axios from 'axios'
+
 
 export default function LoginFrom(){
   const [user,setUser]=useState({})
+  const [message,setMessage]=useState()
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(user)
+    axios.post('http://localhost:4000/login',{user:user})
+    .then(res=>{
+      console.log(res)
+      setMessage(res.data)
+    })
+    .catch(err=>console.log(err))
 
   
   }
@@ -18,13 +28,14 @@ export default function LoginFrom(){
 
     return(
       <>
+      {message}
       <form method="post" onSubmit={handleSubmit}>
       <label>
       <input 
         type="email" 
         name="email" 
         onChange={valueHandler}
-        value=""
+        value={user.email || ""}
       />
       </label>
       <label>Password
@@ -32,7 +43,7 @@ export default function LoginFrom(){
           type="password" 
           name="password" 
           onChange={valueHandler}
-          value=""
+          value={user.password || ""}
         />
         </label>
         <input type="submit" value="Login" />
